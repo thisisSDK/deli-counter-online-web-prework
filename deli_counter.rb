2c04db4804a8f688a6a137cc86b4491b2db76e8f
sdk1270 <sdk1270@gmail.com>
2018-10-19 20:01:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,11 +25,21 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
-
+##### testing
   counter = 0 
 
 def take_a_number(katz_deli)
   counter += 1 
-  katz_deli << counter(1)
+  katz_deli << counter
   puts "Welcome. You are number #{katz_deli.length} in line."
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
